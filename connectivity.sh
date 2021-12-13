527398a98f08a7ed25f6fc75af3ff0768be139f6
Iziram <contact@iziram.fr>
2021-12-13 15:09:20 +0100

    bug fix virgule

--- connectivity.sh
+++ connectivity.sh
@@ -2,7 +2,7 @@ if [ $# -gt 2 ]
 then
     ping=$(ping -c 1 $1 | grep "time=" | cut -d " " -f 8 | tr -d "time=")
     touch /tmp/wget_verbose
-    wget -O /tmp/test $2 -o /tmp/wget_verbose --delete-after
+    wget -O /tmp/test $2 -o /tmp/wget_verbose --delete-after --limit-rate=12000
     download_text=`tail -n 2 /tmp/wget_verbose | cut -d "(" -f2 | cut -d ")" -f1`
     download_number=`echo $download_text | cut -d " " -f1`
     download_suffix=`echo $download_text | cut -d " " -f2`
@@ -11,7 +11,7 @@ then
     then
         download_number=`bc <<< $download_number*1000`
     fi
-
     depoch=$(date +%s)
+    download_number=${download_number/","/"."}
     echo "$ping,$download_number,$depoch" >> $3
 fi
