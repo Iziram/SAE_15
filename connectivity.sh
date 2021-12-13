3cc81c4d66c9f855094d33c4d228a473297c3470
Iziram <contact@iziram.fr>
2021-12-13 14:58:30 +0100

    download fait

--- connectivity.sh
+++ connectivity.sh
@@ -1,7 +1,17 @@
-if [ $# -gt 0 ]
+if [ $# -gt 2 ]
 then
-    p=$(ping -c 1 $1 || echo "null")
-    ping=$("$p" | grep "time=" | cut -d " " -f 8 | tr -d "time=")
+    ping=$(ping -c 1 $1 | grep "time=" | cut -d " " -f 8 | tr -d "time=")
+    touch /tmp/wget_verbose
+    wget -O /tmp/test $2 -o /tmp/wget_verbose --delete-after
+    download_text=`tail -n 2 /tmp/wget_verbose | cut -d "(" -f2 | cut -d ")" -f1`
+    download_number=`echo $download_text | cut -d " " -f1`
+    download_suffix=`echo $download_text | cut -d " " -f2`
+
+    if [ $(echo $download_suffix | grep M -n) ]
+    then
+        download_number=`bc <<< $download_number*1000`
+    fi
+
     depoch=$(date +%s)
-    echo $ping, $depoch
+    echo "$ping,$download_number,$depoch" >> $3
 fi
