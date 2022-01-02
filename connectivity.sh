1c7da472b9bf82254eb3517fb10d68d320fefb97
Iziram <contact@iziram.fr>
2022-01-02 18:21:45 +0100

    avancement

--- connectivity.sh
+++ connectivity.sh
@@ -1,17 +1,8 @@
 if [ $# -gt 2 ]
 then
     ping=$(ping -c 1 $1 | grep "time=" | cut -d " " -f 8 | tr -d "time=")
-    touch /tmp/wget_verbose
-    wget -O /tmp/test $2 -o /tmp/wget_verbose --delete-after --limit-rate=12000
-    download_text=`tail -n 2 /tmp/wget_verbose | cut -d "(" -f2 | cut -d ")" -f1`
-    download_number=`echo $download_text | cut -d " " -f1`
-    download_suffix=`echo $download_text | cut -d " " -f2`
-
-    if [ $(echo $download_suffix | grep M -n) ]
-    then
-        download_number=`bc <<< $download_number*1000`
-    fi
+    download_number=`curl -w '%{speed_download}\n' $2 | tail -n 1`
+    upload_number=`scp -vi iutsshkey iutsshkey iut@iziram.fr:/home/iut/upload |& tail -n 2 | head -n 1 | cut -d " " -f 5`
     depoch=$(date +%s)
-    download_number=${download_number/","/"."}
-    echo "$ping,$download_number,$depoch" >> $3
+    echo "$ping,$download_number,$upload_number$depoch" >> $3
 fi
