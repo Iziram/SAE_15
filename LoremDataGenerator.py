54068968e476c6abdf71bf721a7988594c28a0c3
Iziram <contact@iziram.fr>
2022-01-05 08:58:29 +0100

    lorem

--- LoremDataGenerator.py
+++ LoremDataGenerator.py
@@ -8,7 +8,7 @@
   - Créé par [Prénom] [Nom] le [Date] .
 """
 from os import remove, path
-from random import randint, random
+from random import randint
 def generatePortData(path:str ="./portData.csv"):
     """!
     @brief Cette fonction génère les données csv concernant les ports d'une machine fictive
@@ -23,7 +23,7 @@ def generatePortData(path:str ="./portData.csv"):
     ip : list = ["1.2.3.4","10.1.1.1","0.0.0.0"]
     lines : list = []
     for i in range(5):
-            port : list = [ randint(1, 100) for x in range(randint(1, 100))]
+            port : list = [ randint(1, 65000) for x in range(randint(1, 100))]
             for p in port :
                 lines.append(f'{ip[randint(0, 2)]},{p},{protocol[randint(0,len(protocol)-1)]},{time}\n')
             time += 60
@@ -54,7 +54,7 @@ def generateConnectivityData(path : str = "./connectivityData.csv"):
     lines : list = []
     
     for i in range(100):
-        lines.append(f'{random() * 10},{random() * 10},{randint(1, 2000)},{time}\n')
+        lines.append(f'{randint(1, 2000)},{randint(1, 2000)},{randint(1, 2000)},{time}\n')
         time += 60
     with open(path,'w') as file :
         file.writelines(lines)
